1983a7bd69860f990fb3e9993d5826b742b2581f
David Kuda <david.kuda@alexanderthamm.com>
2022-03-09 15:17:12 +0000

    Iterate sampleData to populate elements

---
+++ components/Game.js
@@ -0,0 +1,94 @@
+import WordleRow from "./WordleRow";
+
+export default function Game(props) {
+  const sampleData = JSON.parse(sampleJson);
+  return (
+    <div className="m-16 max-w-4xl">
+      <div className="game-container bg-slate-300 p-12 rounded-xl drop-shadow-lg">
+        {sampleData.history.map((row, rowNum) => (
+          <WordleRow key={`row_${rowNum}`} rowNum={rowNum} letters={row.letters} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const sampleJson = `
+{
+  "game_won": "True",
+  "history": [
+    {
+      "letters": [
+        {
+          "letter": "E",
+          "state": -1
+        },
+        {
+          "letter": "V",
+          "state": 1
+        },
+        {
+          "letter": "A",
+          "state": 1
+        },
+        {
+          "letter": "D",
+          "state": -1
+        },
+        {
+          "letter": "E",
+          "state": -1
+        }
+      ]
+    },
+    {
+      "letters": [
+        {
+          "letter": "F",
+          "state": -1
+        },
+        {
+          "letter": "O",
+          "state": -1
+        },
+        {
+          "letter": "C",
+          "state": -1
+        },
+        {
+          "letter": "A",
+          "state": 2
+        },
+        {
+          "letter": "L",
+          "state": 2
+        }
+      ]
+    },
+    {
+      "letters": [
+        {
+          "letter": "N",
+          "state": 2
+        },
+        {
+          "letter": "A",
+          "state": 2
+        },
+        {
+          "letter": "V",
+          "state": 2
+        },
+        {
+          "letter": "A",
+          "state": 2
+        },
+        {
+          "letter": "L",
+          "state": 2
+        }
+      ]
+    }
+  ]
+}
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
-import WordleRow from "../components/WordleRow";
+import Game from "../components/Game";
 
 export default function Home() {
   return (
     <div className="m-16 max-w-4xl">
-      <div className="game-container bg-slate-300 p-12 rounded-xl drop-shadow-lg">
-        <h1 className="text-3xl font-medium text-center drop-shadow">
-          Play Wordle with an AI
-        </h1>
-        <WordleRow
-          letter_1="A"
-          letter_2="B"
-          letter_3="C"
-          letter_4="D"
-          letter_5="E"
-        />
-        <WordleRow
-          letter_1="A"
-          letter_2="B"
-          letter_3="C"
-          letter_4="D"
-          letter_5="E"
-        />
-        <WordleRow
-          letter_1="B"
-          letter_2="R"
-          letter_3="O"
-          letter_4="D"
-          letter_5="E"
-        />
-      </div>
+      <h1 className="text-base md:text-3xl font-medium text-center drop-shadow">
+        Play Wordle with an AI
+      </h1>
+      <Game />
     </div>
   );
 }
