eda8ab2bf6f0f312ef21e9779b5570031f6d9c80
David Kuda <david.kuda@pm.me>
2022-03-10 16:12:44 +0100

    fix: Check if data is available before rendering

--- components/Game.js
+++ components/Game.js
@@ -17,7 +17,7 @@ export default function Game(props) {
   return (
     <div className="m-16 max-w-4xl">
       <div className="game-container bg-slate-300 p-12 rounded-xl drop-shadow-lg">
-        {data.history.map((row, rowNum) => (
+        {data && data.history.map((row, rowNum) => (
           <WordleRow key={`row_${rowNum}`} rowNum={rowNum} letters={row.letters} />
         ))}
       </div>
