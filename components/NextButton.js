f59755395fa22614f7140e316aaf6d7642c130a6
David Kuda <david.kuda@pm.me>
2022-03-10 23:37:56 +0100

    Make the "NextButton" slightly wider

--- components/NextButton.js
+++ components/NextButton.js
@@ -15,7 +15,7 @@ export default function NextButton(props) {
   return (
     <button
       onClick={handleClick}
-      className="w-24 self-center mt-8 bg-gray-500  border-gray-500 text-sm border-4 text-white py-1 px-2 rounded hover:bg-gray-700 hover:border-gray-700"
+      className="w-28 self-center mt-8 bg-gray-500  border-gray-500 text-sm border-4 text-white py-1 px-2 rounded hover:bg-gray-700 hover:border-gray-700"
     >
       Next Guess
     </button>
