7096280fe7a552ab5b6109f5db3e18985d4ae82f
David Kuda <david.kuda@pm.me>
2022-03-11 00:23:17 +0100

    Render question mark, not exclamation mark

--- components/PlayAgainButton.js
+++ components/PlayAgainButton.js
@@ -8,7 +8,7 @@ export default function PlayAgainButton({ resetStates }) {
       className="w-28 self-center mt-8 bg-gray-500  border-gray-500 text-sm border-4 text-white py-1 px-2 rounded hover:bg-gray-700 hover:border-gray-700"
       onClick={handleClick}
     >
-      Play Again!
+      Play Again?
     </button>
   );
 }
