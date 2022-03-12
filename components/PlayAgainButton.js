21cb4c9c7945e91e60a73248f5cf5846b8c3cbd4
David Kuda <david.kuda@pm.me>
2022-03-12 22:50:54 +0100

    Enable click on play-again-button with keyup a

--- components/PlayAgainButton.js
+++ components/PlayAgainButton.js
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
+
 export default function PlayAgainButton({ resetStates }) {
   function handleClick(event) {
     resetStates();
   }
 
+  useEffect(() => {
+    // Add event listeners
+    const upHandler = (event) => {
+      if (event.key === "a") {
+        document.getElementById("play-again-button").click();
+      }
+    };
+    window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
   return (
     <button
-      className="w-28 self-center mt-8 bg-gray-500  border-gray-500 text-sm border-4 text-white py-1 px-2 rounded hover:bg-gray-700 hover:border-gray-700"
+      id="play-again-button"
+      className="w-auto self-center mt-8 bg-gray-500  border-gray-500 text-sm border-4 text-white py-1 px-2 rounded hover:bg-gray-700 hover:border-gray-700"
       onClick={handleClick}
     >
       Play Again?
