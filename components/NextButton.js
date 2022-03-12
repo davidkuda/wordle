0a113502c9008894c6ef4e791332dd7bb454ad08
David Kuda <david.kuda@pm.me>
2022-03-12 22:49:27 +0100

    Enable click on next-button with keyup of n

--- components/NextButton.js
+++ components/NextButton.js
@@ -1,12 +1,29 @@
-export default function NextButton({dequeueGameData}) {
+import { useEffect } from "react";
+
+export default function NextButton({ dequeueGameData }) {
   function handleClick(event) {
     dequeueGameData();
   }
 
+  useEffect(() => {
+    // Add event listeners
+    const upHandler = (event) => {
+      if (event.key === "n") {
+        document.getElementById("next-guess-button").click();
+      }
+    };
+    window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
   return (
     <button
+      id="next-guess-button"
       onClick={handleClick}
-      className="w-28 self-center mt-8 bg-gray-500  border-gray-500 text-sm border-4 text-white py-1 px-2 rounded hover:bg-gray-700 hover:border-gray-700"
+      className="w-auto self-center mt-8 bg-gray-500  border-gray-500 text-sm border-4 text-white py-1 px-2 rounded hover:bg-gray-700 hover:border-gray-700"
     >
       Next Guess
     </button>
